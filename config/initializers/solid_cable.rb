69ecc08135698a9d820e52156f0ba8d22ccafdad
iormg88-eng <ior.mg88@gmail.com>
2026-03-29 15:02:32 +0900

    Remove solid_cable

--- config/initializers/solid_cable.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.production?
-  Rails.application.config.after_initialize do
-    if defined?(SolidCable)
-      ActionCable.server.config.cable = { "adapter" => "async" }
-    end
-  end
-end
